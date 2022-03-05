a23ed4a62cd91917e894df003f1a0fe58cfc9f41
han254 <peternjenga1706@gmail.com>
2022-03-05 22:50:43 +0300

    add an alert to display invalid year input

---
+++ Javascript/script.js
@@ -0,0 +1,28 @@
+//Form validation
+var YY, MM, DD, d, dayvalue;
+var weekDays = [
+  "Sunday",
+  "MOnday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Sartuday",
+];
+var akanMales = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var akanFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function validateForm(e) {
+     e.preventDefault(e)
+  var gender = document.querySelector("input[name='gender']:checked");
+  var day =document.myForm.date.value;
+  if (
+    document.myForm.year.value == "" ||
+    document.myForm.year.value.length != 4 ||
+    document.myForm.year.value > 2022 ||
+    document.myForm.year.value < 1900
+  ) {
+    alert("Please Enter a valid Year Of Birth");
+    document.myForm.year.focus();
+    return false;
+ 
